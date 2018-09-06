7e786726dfb5ce153f66d122bcb751484cbc96a4
aainn <inana_@gmx.de>
2018-09-06 11:34:23 +0200

    changed the templateUrl property to template and added comments

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  /* the selector property can be omited */
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  /* template or templateUrl  is a mandatory property.
+  templateUrl is used for an external file, template for an internal template */
+  template: '<app-server></app-server><app-server></app-server>',
+  /* the template-line musn't be wrapped when you've used single quotes.
+  (not supporrted in default typescript strings)
+  in case you want to add multiple lines, use backticks! */
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
